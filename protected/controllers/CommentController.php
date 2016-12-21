4b2618730bccfc73027f443c4274c1ed9ac23e5c
akash10arora <akash@hitwicket.com>
2016-12-21 20:00:05 +0530

    abc

--- protected/controllers/CommentController.php
+++ protected/controllers/CommentController.php
@@ -1,57 +1,66 @@
 <?php
 class CommentController extends Controller {
 
-  public function actionCreate() {
-    if(isset($_POST['Comment'])) {
-      $comment = Comment::create($_POST['Comment']);
-      if(!$comment->errors) {
-        $this->renderSuccess(array('post_id'=>$comment->post_id,'content'=>$comment->content,'user_id'=>$comment->user_id));
-      } else {
-        $this->renderError($this->getErrorMessageFromModelErrors($comment));
-      }    
-    } else {
-      $this->renderError('Please send comment data!');
-    }
-  }
+	public function actionCreate() {
+		if(isset($_POST['Comment'])) {
+			$comment = Comment::create($_POST['Comment']);
+			if(!$comment->errors) {
+				$this->renderSuccess(array('post_id'=>$comment->post_id,'content'=>$comment->content,'user_id'=>$comment->user_id));
+			} else {
+				$this->renderError($this->getErrorMessageFromModelErrors($comment));
+			}    
+		} else {
+			$this->renderError('Please send comment data!');
+		}
+	}
 
-    public function actionComments($id) {                   //returns only the particular comment
-      $post = Post::model()->findByPk($id);
-      $comments = $post->comments;
-      foreach ($comments as $comment) {
-        
-           // echo $comment->content;
+	public function actionComments($id) {                   //returns only the particular comment
+		$post = Post::model()->findByPk($id);
+		$comments = $post->comments;
+		foreach ($comments as $comment) {
+			
+		   // echo $comment->content;
 
-            echo CJSON::encode(array('status'=>'SUCCESS', 'comment'=>$comment->content));  // content is attribute
-           // echo "<br>";
+			echo CJSON::encode(array('status'=>'SUCCESS', 'comment'=>$comment->content));  // content is attribute
+		   // echo "<br>";
 
-          }
-        }
-    public function actionTopComments($id){                                         //returns top latest comment and the persons name 
+		}
+	}
+	public function actionTopComments($id){                                         //returns top latest comment and the persons name 
 
-     $commentss = Comment::model()->findAll(array('condition'=>"post_id = :post_id", 'params'=>array('post_id'=>$id), 'order'=>'created_at DESC', 'limit'=>5));
-     $comments_data = array();
-     foreach($commentss as $comment){
-       $comments_data[] = array('user_name'=>$comment->user->name, 'content'=>$comment->content);
-     }
-     echo CJSON::encode(array('status'=>'SUCCESS', 'Comments_information'=>$comments_data));
-   }
+		$commentss = Comment::model()->findAll(array('condition'=>"post_id = :post_id", 'params'=>array('post_id'=>$id), 'order'=>'created_at DESC', 'limit'=>5));
+		$comments_data = array();
+		foreach($commentss as $comment){
+			$comments_data[] = array('user_name'=>$comment->user->name, 'content'=>$comment->content);
+		}
+		echo CJSON::encode(array('status'=>'SUCCESS', 'Comments_information'=>$comments_data));
+	}
 
    public function actionDelete($id){                  //delete post
 
-     $post = Post::model()->findByPk($id);
-     $post->status = 2;
-     $post->save();
+	$post = Post::model()->findByPk($id);
+	$post->status = 2;
+	$post->save();
    }
 
    public function actionRecover($id){                  //activate post
 
-     $post = Post::model()->findByPk($id);
-     $post->status = 1;
-     $post->save();
+	$post = Post::model()->findByPk($id);
+	$post->status = 1;
+	$post->save();
+   }
+   public function actionUpdate($str, $id){
+
+	$post = Post::model()->findByPk($id);
+	if($post->status == 1){
+
+		$post->content = $str;
+		$post->save();
+	}
    }
 
    
- }
+}
 
 
 
--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -2,61 +2,61 @@
 <?php
 class UserController extends Controller {
 
-    public function actionCreate() {
-        if(isset($_POST['new_user'])) {
-            $new_user = User::create($_POST['new_user']);
-            if(!$new_user->errors) {
-                $this->renderSuccess(array('user_id'=>$new_user->id,'name'=>$new_user->name,'password'=>$new_user->password,'email'=>$new_user->email));
-            } else {
-                $this->renderError($this->getErrorMessageFromModelErrors($new_user));
-            }
-        } else {
-            $this->renderError('Please send User data proplerly !');
-        }
-    }
+	public function actionCreate() {
+		if(isset($_POST['new_user'])) {
+			$new_user = User::create($_POST['new_user']);
+			if(!$new_user->errors) {
+				$this->renderSuccess(array('user_id'=>$new_user->id,'name'=>$new_user->name,'password'=>$new_user->password,'email'=>$new_user->email));
+			} else {
+				$this->renderError($this->getErrorMessageFromModelErrors($new_user));
+			}
+		} else {
+			$this->renderError('Please send User data proplerly !');
+		}
+	}
 
-    public function actionView($id) {						//view post by id
+		public function actionView($id) {						//view post by id
 
-      $view =  Post::model()->findByPk($id);
-      if(!$view){
-        //empty
-      }   else {
+			$view =  Post::model()->findByPk($id);
+			if(!$view){
+				//empty
+			}   else {
 
-         echo CJSON::encode(array('status'=>'SUCCESS','id'=>$view->id,'content'=>$view->content));   
-     }
- }
+				echo CJSON::encode(array('status'=>'SUCCESS','id'=>$view->id,'content'=>$view->content));   
+			}
+		}
 
  public function actionViewNewsfeed($id) {												//view all posts
-          $posts = Post::model()->findAllByAttributes(array('user_id'=>$id));
-           $posts_data = array();
-           foreach ($posts as $post) {
-           $posts_data[] = array('id'=>$post->id, 'content'=>$post->content);
-           }
-         echo CJSON::encode(array('status'=>'SUCCESS',
-            'posts_data'=>$posts_data
-         ));
-}
+ 	$posts = Post::model()->findAllByAttributes(array('user_id'=>$id));
+ 	$posts_data = array();
+ 	foreach ($posts as $post) {
+ 		$posts_data[] = array('id'=>$post->id, 'content'=>$post->content);
+ 	}
+ 	echo CJSON::encode(array('status'=>'SUCCESS',
+ 		'posts_data'=>$posts_data
+ 		));
+ }
 
 /*
 public function actionNewsfeeds(){
 
-   echo "News Feeds </br>";
-   //$posts = Post::model()->findAllByAttributes(array('user_id'=>$id));
-   //$posts = Post::model()->findAll("user_id = :user_id", array('user_id'=>$id));
-   //$posts = Post::model()->findAll(array('condition'=>"user_id = :user_id", 'params'=>array('user_id'=>$id)));
-   $posts = Post::model()->findAll(array('condition'=>"user_id != :user_id", 'params'=>array('user_id'=>$id), 'order'=>'created_at DESC', 'limit'=>10));
+	 echo "News Feeds </br>";
+	 //$posts = Post::model()->findAllByAttributes(array('user_id'=>$id));
+	 //$posts = Post::model()->findAll("user_id = :user_id", array('user_id'=>$id));
+	 //$posts = Post::model()->findAll(array('condition'=>"user_id = :user_id", 'params'=>array('user_id'=>$id)));
+	 $posts = Post::model()->findAll(array('condition'=>"user_id != :user_id", 'params'=>array('user_id'=>$id), 'order'=>'created_at DESC', 'limit'=>10));
 
-   if(!$posts) {
+	 if(!$posts) {
 
 
-   }
-   else{
+	 }
+	 else{
 
-       $posts_data = array();
+			 $posts_data = array();
 
-       foreach ($posts as $post) {
-         $posts_data[] = array('id'=>$post->id, //'content'=>$post->content, 
-         'user_name'=>$post->user->name);
+			 foreach ($posts as $post) {
+				 $posts_data[] = array('id'=>$post->id, //'content'=>$post->content, 
+				 'user_name'=>$post->user->name);
 }
 echo CJSON::encode(array('status'=>'SUCCESS',
  'posts_data'=>$posts_data
@@ -66,53 +66,61 @@ echo CJSON::encode(array('status'=>'SUCCESS',
 
 
 public function actionProfile($id) {                //user profile by id
-   $user = User::model()->findbyPK($id);
-   if(!$user) {
-       echo "Account does not exist.";
+	$user = User::model()->findbyPK($id);
+	if(!$user) {
+		echo "Account does not exist.";
 
-   }
-   else {
-      echo CJSON::encode(array('status'=>'SUCCESS','name'=>$user->name,'email'=>$user->email));
-  }
+	}
+	else {
+		echo CJSON::encode(array('status'=>'SUCCESS','name'=>$user->name,'email'=>$user->email));
+	}
 }
 
-  public function actionLogin($id) {
-        $user = User::model()->findbyPK($id);
-        if(!$user) {
-            echo "Account does not exist.";
-        }
-        else {
-        //    echo 'hi';
-           echo CJSON::encode(array('id'=>$user->id,'status'=>'SUCCESS'));
-       }
-   }
+public function actionLogin($id) {
+	$user = User::model()->findbyPK($id);
+	if(!$user) {
+		echo "Account does not exist.";
+	}
+	else {
+				//    echo 'hi';
+		echo CJSON::encode(array('id'=>$user->id,'status'=>'SUCCESS'));
+	}
+}
 
 
-   public function actionSearchProfile($name){
-       $users = User::model()->findAllByAttributes(array('name'=>$name));
-       if(!$user) {
-            echo "Account does not exist.";
-        }
-        else{
-       $users_profile = array();
-       foreach($users as $user){
-           $users_profile[] = array('user_id'=>$user->id, 'user_name'=>$user->name, 'email'=>$user->email);
-       }
-   }
-       echo CJSON::encode(array('status'=>'SUCCESS', 'users_profile'=>$users_profile));
-   }
+public function actionSearchProfile($name){
+	$users = User::model()->findAllByAttributes(array('name'=>$name));
+	if(!$user) {
+		echo "Account does not exist.";
+	}
+	else{
+		$users_profile = array();
+		foreach($users as $user){
+			$users_profile[] = array('user_id'=>$user->id, 'user_name'=>$user->name, 'email'=>$user->email);
+		}
+	}
+	echo CJSON::encode(array('status'=>'SUCCESS', 'users_profile'=>$users_profile));
+}
  public function actionDelete($id){                  //delete user
 
-       $users = User::model()->findByPk($id);
-       $users->status = 2;
-       $users->save();
-   }
+ 	$users = User::model()->findByPk($id);
+ 	$users->status = 2;
+ 	$users->save();
+ }
 
-   public function actionRecover($id){                  //activate user
+	 public function actionRecover($id){                  //activate user
 
-       $users = User::model()->findByPk($id);
-       $users->status = 1;
-       $users->save();
-   }
+	 	$users = User::model()->findByPk($id);
+	 	$users->status = 1;
+	 	$users->save();
+	 }
+	 public function actionUpdate($str, $id){
 
-}
+	 	$post = Post::model()->findByPk($id);
+	 	if($post->status == 1){
+
+	 		$post->content = $str;
+	 		$post->save();
+	 	}
+	 }
+	}
